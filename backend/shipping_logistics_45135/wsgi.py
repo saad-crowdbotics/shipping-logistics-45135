f7820314557d7c27e12b4f74a93a6df611946a89
Crowdbotics <team@crowdbotics.com>
2023-12-20 17:01:55 +0000

    Initial commit

---
+++ backend/shipping_logistics_45135/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for shipping_logistics_45135 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "shipping_logistics_45135.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shipping-logistics-45135",
+  url: "https://shipping-logistics-45135.botics.co",
+  api: "https://shipping-logistics-45135.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
